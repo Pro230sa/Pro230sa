88879b413eb7f8eaca7b9642775a6cf0db4f094e
ra131 <ragad2020o@gmail.com>
2022-04-16 21:05:23 +0300

    A

--- resources/views/admin_views/reservation_management/index.blade.php
+++ resources/views/admin_views/reservation_management/index.blade.php
@@ -3,32 +3,36 @@
 @section('content')
 <div class="container pt-5">
     <div class="row">
+    <!-- mm 
         <div class="col-sm-3 p-0">
             {{-- <a href="{{route('admin.reservation_times.create')}}" class="btn btn-success">Create Reservation Time</a> --}}
         </div>
+          -->
     </div>
     <div class="row mt-3">
         <table class="table">
             <thead>
                 <tr>
                     <th>#</th>
-                    <th>Title</th>
-                    <th>Start Date</th>
-                    <th>End Date</th>
-                    <th>Fee</th>
-                    <th>Note</th>
-                    <th>Actions</th>
+                    <th>Cupboard</th>
+                    <th>Locker</th>
+                    <th>Reservation Time</th>
+                    <th>Name</th>
+                    <th>Phone Number</th>
+                    <th>Fee Receipt</th>
+                    <th>key of Locker </th>
+                    <th>status</th>
                 </tr>
             </thead>
             <tbody>
                 {{-- @foreach ($reservation_times as $key => $reservation_time)
                     <tr>
                         <th scope="row">{{$key+1}}</th>
-                        <td>{{$reservation_time->title}}</td>
-                        <td>{{$reservation_time->start_date}}</td>
-                        <td>{{$reservation_time->end_date}}</td>
-                        <td>{{$reservation_time->locker_fee}}</td>
-                        <td>{{$reservation_time->notes}}</td>
+                        <td>{{$cupboards->number}}</td>
+                        <td>{{$lockers->locker_number}}</td>
+                        <td>{{$users->name}}</td>
+                        <td>{{$users->phone_number}}</td>
+                        <td>{{$reservations->status}}</td>
                         <td>
                             <a href="{{route('admin.reservation_times.edit', $reservation_time->id)}}" class="btn btn-outline-secondary"><i class="fa fa-edit fa-fw"></i></a>
                         </td>
